9841b135b1c2cdb607b6898fe6c04b2c0bd6818f
mauelcg <gmarklemuel@gmail.com>
2026-01-17 19:53:53 +0800

    Remove ownership rights

--- windows/established_connection_processes.ps1
+++ windows/established_connection_processes.ps1
@@ -1,7 +1,3 @@
-# Source - https://stackoverflow.com/q
-# Posted by luckman212, modified by community. See post 'Timeline' for change history
-# Retrieved 2026-01-17, License - CC BY-SA 3.0
-
 # Build process lookup hashtable for fast O(1) lookups
 $processLookup = @{}
 Get-Process -IncludeUserName | Where-Object UserName | ForEach-Object {
